1898f43a11702167e20b098a92bd6188a2ecc8bd
kuronn0410 <yamayuu7745@gmail.com>
2026-04-26 17:09:39 +0900

    時間

--- Assets/Images/3Dモデル用script/TimeText.cs
+++ Assets/Images/3Dモデル用script/TimeText.cs
@@ -15,6 +15,9 @@ public class TimeText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeText.text = Timer.timer.ToString("F2");
+
+        int minutes = (int)(Timer.timer / 60);
+        int seconds = (int)(Timer.timer % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
--- Assets/Scenes/script/BestTime.cs
+++ Assets/Scenes/script/BestTime.cs
@@ -11,6 +11,8 @@ public class BestTime : MonoBehaviour
     {
         // ★保存されたベストタイムを取得して表示
         float best = ScoreManager.GetBestTime();
-        bestTimeText.text = "Best Time: " + best.ToString("F2") + "s";
+        int minutes = (int)(best / 60);
+        int seconds = (int)(best % 60); 
+        bestTimeText.text = string.Format("Best Time: {0:00}:{1:00}", minutes, seconds);
     }
 }
